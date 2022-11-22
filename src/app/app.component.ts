daa7600e34ad34923d6706df95c3ddd24af8e0c5
son14071999 <50294838+son14071999@users.noreply.github.com>
2022-11-22 09:46:36 +0700

    use interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Product } from './common/product'
+import { Inventor } from './common/inventor'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-project';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.render(this.show)
+  }
+  product: Product = {
+    name: 'Hat',
+    quantity: 12,
+    price: 200
+  }
+
+  inventors: Inventor[] = [
+    {id:1, first: 'nguyen', last: 'son', year: 1999, passed: 2100},
+    {id:2, first: 'nguyen', last: 'son', year: 1999, passed: 2100},
+    {id:3, first: 'nguyen', last: 'son', year: 1999, passed: 2100},
+  ]
+
+
+  show(innerHTML: string) {
+    let item = document.querySelector('#list')
+    if(item) {
+      item.innerHTML = innerHTML
+    }
+  }
+
+  render(callback: (data: string) => any) {
+    let innerHTML = this.inventors.map(item => {
+      return `
+      <tr>
+          <td>${item.id}</td>
+          <td>${item.first}</td>
+          <td>${item.last}</td>
+          <td>${item.year}</td>
+          <td>${item.passed}</td>
+      </tr>
+      `
+    }).join('')
+    callback(innerHTML)
+  }
 }
